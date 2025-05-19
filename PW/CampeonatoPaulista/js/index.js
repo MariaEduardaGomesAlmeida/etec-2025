93df0edf6b5c61ceb0aefa2082aa4f66c49a0894
Maria Eduarda <143905191+MariaEduardaGomesAlmeida@users.noreply.github.com>
2025-05-19 11:39:53 -0300

    Update index.js

--- PW/CampeonatoPaulista/js/index.js
+++ PW/CampeonatoPaulista/js/index.js
@@ -2,30 +2,22 @@ import { times } from './times.js';
 
 console.log(times)
 
-let trs = document.querySelectorAll('tr')
-let filhosDaLinha1 = trs[1].children
-
-let col1_1 = filhosDaLinha1[0]
-let col1_2 = filhosDaLinha1[1]
-let col1_3 = filhosDaLinha1[2]
-let col1_4 = filhosDaLinha1[3]
-let col1_5 = filhosDaLinha1[4]
-let col1_6 = filhosDaLinha1[5]
-let col1_7 = filhosDaLinha1[6]
-let col1_8 = filhosDaLinha1[7]
-let col1_9 = filhosDaLinha1[8]
-let col1_10 = filhosDaLinha1[9]
 
 
+let trs = document.querySelectorAll('tr');
 
-col1_1.textContent = times[0].time
-col1_2.textContent = times[0].pontos
-col1_3.textContent = times[0].jogos
-col1_4.textContent = times[0].vitorias
-col1_5.textContent = times[0].empates
-col1_6.textContent = times[0].derrotas
-col1_7.textContent = times[0].gols_pro
-col1_8.textContent = times[0].gols_contra
-col1_9.textContent = times[0].saldo_gols
-col1_10.textContent = times[0].porcentagem
+for (let i = 1; i < trs.length && i - 1 < times.length; i++) {
+    let filhos = trs[i].children;
+    let time = times[i - 1]; 
 
+    filhos[0].textContent = time.time;
+    filhos[1].textContent = time.pontos;
+    filhos[2].textContent = time.jogos;
+    filhos[3].textContent = time.vitorias;
+    filhos[4].textContent = time.empates;
+    filhos[5].textContent = time.derrotas;
+    filhos[6].textContent = time.gols_pro;
+    filhos[7].textContent = time.gols_contra;
+    filhos[8].textContent = time.saldo_gols;
+    filhos[9].textContent = time.porcentagem + '%';
+}
